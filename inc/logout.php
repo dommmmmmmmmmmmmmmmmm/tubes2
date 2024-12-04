b28a06608aa6512b4b5cb9f3e9a5432193c796af
IlyasFadillah <127228181+dommmmmmmmmmmmmmmmmm@users.noreply.github.com>
2024-12-04 22:25:07 +0700

    lsdkwmd

--- inc/logout.php
+++ inc/logout.php
@@ -13,7 +13,7 @@ session_destroy();
         // Menampilkan alert saat logout
         alert("Anda telah logout.");
         // Redirect ke halaman login setelah alert
-        window.location.href = "../login.php"; // Ganti dengan URL halaman login Anda
+        window.location.href = "../utama.php"; // Ganti dengan URL halaman login Anda
     </script>
 </head>
 <body>
--- inc/proslog.php
+++ inc/proslog.php
@@ -3,35 +3,27 @@
   include 'koneksi.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
-$sql = mysqli_query($koneksi, "SELECT * FROM petugas WHERE username='$username' AND password='$password' ");
+$sql = mysqli_query($koneksi, "SELECT * FROM akun WHERE username='$username' AND password='$password' ");
 $data = mysqli_fetch_array($sql);
 $cek = mysqli_num_rows($sql);
 if ($cek>0) {
-  if ($data['level']=="admin") {
+  if ($data['role']=="pengguna") {
     $_SESSION['username']= $username;
-    $_SESSION['status']= "ADMIN";
-    $_SESSION['id_petugas']= $data[0];
-    header("location:../admin/index.php");
+    $_SESSION['status']= "pengguna";
+    $_SESSION['id_akun']= $data[0];
+    header("location:pengguna/beranda.html");
 
-  }else if ($data['level']=="operator") {
+  }else if ($data['role']=="perusahaan") {
     $_SESSION['username']= $username;
-    $_SESSION['status']= "OPERATOR";
-    $_SESSION['id_petugas']= $data[0];
-    header("location:../operator/index.php"); 
-
-
-}else if ($data['level']=="peminjam") {
-    $_SESSION['username']= $username;
-    $_SESSION['status']= "PEMINJAM";
-    $_SESSION['id_petugas']= $data[0];
-    header("location:../peminjam/index.php");
-}
-
+    $_SESSION['status']= "perusahaan";
+    $_SESSION['id_akun']= $data[0];
+    header("location:perusahaan/dashboard.php"); 
+  }
 }else{
   ?>
   <script type="text/javascript">
     alert("Akun Anda Ada Yang Salah");
-    window.location="../login.php";
+    window.location="loginpengguna.php";
   </script>
 
 <?php 
